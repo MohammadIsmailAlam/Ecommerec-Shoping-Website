bb6fe4f1c70fe0f8a767f9f7ae45dccbc74cd53f
Mohammad Ismail Alam <ismailalam901@gmail.com>
2023-08-18 11:23:58 +0600

    Route set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
-import {
-  BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Nav from "./Components/nav";
+import Home from "./Pages/home";
+import Product from "./Pages/product";
+import About from "./Pages/about";
+import Contact from "./Pages/contact";
+import Cart from "./Pages/cart";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Nav />
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
       </div>
     </Router>
   );
