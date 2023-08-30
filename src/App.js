d94168ca00f3a652af25f759b234e962b1b01670
Mohammad Ismail Alam <ismailalam901@gmail.com>
2023-08-30 21:33:21 +0600

    Modal added to show the product deatils

--- src/App.js
+++ src/App.js
@@ -6,23 +6,49 @@ import Contact from "./Pages/Contact";
 import About from "./Components/Home/About";
 import Footer from "./Components/Footer";
 import ProductList from "./Pages/ProductList";
+import ProductDetails from "./Assets/ProductDetails";
+import Model from "./Components/Model";
 
 export const userContext = React.createContext(null);
 
 const App = () => {
   const [userEmail, setUserEmail] = useState("");
+  
+  const [product, setProduct] = useState(ProductDetails);
+  const searchBtn = (searchTerm) => {
+    const filteredProducts = ProductDetails.filter((product) =>
+      product.Title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setProduct(filteredProducts);
+  };
+
+  const [close, setClose] = useState(false)
+  const [detail, setDetail] = useState([])
+
+  const view = (product) => 
+  {
+    setDetail([{...product}])
+    setClose(true)
+  }
+
   return (
     <>
       <userContext.Provider value={{ userEmail, setUserEmail }}>
         <Router>
-          <Nav />
+          <Nav searchBtn={searchBtn} />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/product" element={<ProductList/>} />
+            <Route
+              path="/product"
+              element={
+                <ProductList product={product} setProduct={setProduct} view={view}/>
+              }
+            />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
           <Footer />
+          <Model close={close} detail={detail} onClose={() => setClose(false)} />
         </Router>
       </userContext.Provider>
     </>
