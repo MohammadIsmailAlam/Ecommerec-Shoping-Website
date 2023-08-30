5d1c56995820f4f3a4af2bfd3f89cd5f2ddd6700
Mohammad Ismail Alam <ismailalam901@gmail.com>
2023-08-30 21:35:04 +0600

    button hover text added

--- src/Components/auth/LogOut.js
+++ src/Components/auth/LogOut.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { CiLogout } from "react-icons/ci";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
+  const [showTooltip, setShowTooltip] = useState(false);
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-      <CiLogout/>
-    </button>
+    <div className="relative">
+      <button
+        onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+        className="relative group"
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
+        <CiLogout />
+      </button>
+      <div className={`absolute top-full left-1/2 transform -translate-x-1/2 ${showTooltip ? "block" : "hidden"}`}>
+        <span className="bg-gray-800 text-white text-sm px-2 py-1 rounded-md whitespace-nowrap">
+          Log Out
+        </span>
+      </div>
+    </div>
   );
 };
 
--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { CiLogin } from "react-icons/ci";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
+  const [showTooltip, setShowTooltip] = useState(false);
 
-  return <button onClick={() => loginWithRedirect()}><CiLogin/></button>;
+  return (
+    <div className="relative">
+      <button
+        onClick={() => loginWithRedirect()}
+        className="relative group"
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
+        <CiLogin />
+      </button>
+      <div className={`absolute top-full left-1/2 transform -translate-x-1/2 ${showTooltip ? "block" : "hidden"}`}>
+        <span className="bg-gray-800 text-white text-sm px-2 py-1 rounded-md ">
+          LogIn
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default LoginButton;
