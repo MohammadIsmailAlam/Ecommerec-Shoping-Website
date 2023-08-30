08bcd2d2bbb4ff12497efefceb9f71e81851fb59
Mohammad Ismail Alam <ismailalam901@gmail.com>
2023-08-30 21:34:32 +0600

    Style changed

--- src/Components/Home/TopBanner.js
+++ src/Components/Home/TopBanner.js
@@ -8,13 +8,16 @@ const TopBanner = () => {
   return (
     <div className="top-banner bg-blue-100 py-8">
       <div className="container flex justify-between">
-        <div className="px-8 details">
-          <h2 className="text-4xl font-semibold text-blue-800">
+        <div className="px-8 detail">
+          <h2 className="text-4xl font-semibold text-blue-800 mb-6">
             The Note Book Collection 2023
           </h2>
-          <button onClick={() => navigate("/product")} className="btn-shopNow">
+          <button
+            onClick={() => navigate("/product")}
+            className="btn-shopNow bg-white text-blue-800 py-2 px-4 rounded-full hover:bg-blue-800 hover:text-white transition duration-300"
+          >
             Shop Now
-            <BsArrowRight className="text-blue-800 ml-2 mt-1" />
+            <BsArrowRight className="ml-2 mt-1" />
           </button>
         </div>
         <div className="img_box">
